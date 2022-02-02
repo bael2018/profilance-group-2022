c1ee52f83e550e333596f3a28408dbd5bbe5f3a4
bael <osh.kg1819@gmail.com>
2022-02-02 11:42:52 +0600

    error page resolved

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,12 +1,16 @@
 
 const initStyles = {
-    
+    width: '100%',
+    heigth: '350px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignContent: 'center'
 }
 
 const Error = () => {
     return (
-        <div>
-            Error
+        <div style={initStyles}>
+            Page not Found | 404
         </div>
     )
 }
